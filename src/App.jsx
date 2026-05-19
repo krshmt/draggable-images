c81ca13e2f0e69357bcef80dfca53bd56a8a785f
krshmt <tourekris16@gmail.com>
2026-05-19 17:43:54 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import DraggableImagesCode from './components/draggable-images'
+
+function App() {
+  return (
+    <>
+      <DraggableImagesCode />
+    </>
+  )
+}
+
+export default App
